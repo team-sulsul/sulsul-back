201c20c36538b9c6ccf06659d763e89d86799d69
Taemin Lee <104557007+denmark-dangnagui@users.noreply.github.com>
2024-01-11 16:46:53 +0900

    VER_0

---
+++ src/test/java/main/sulsul/SulsulApplicationTests.java
@@ -0,0 +1,13 @@
+package main.sulsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
