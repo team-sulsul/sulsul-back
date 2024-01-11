d44eaf98120017453c653ca7b3de1d0910a6d406
Taemin Lee <104557007+denmark-dangnagui@users.noreply.github.com>
2024-01-11 16:41:08 +0900

    ver.0

---
+++ sulsul/src/main/java/main/sulsul/SulsulApplication.java
@@ -0,0 +1,13 @@
+package main.sulsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulsulApplication.class, args);
+	}
+
+}
