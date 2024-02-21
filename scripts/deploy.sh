a9ea0c0c248ad5eba7fbe66570e22afa9cb8ec91
somefood <iamseokjuhong@gmail.com>
2024-02-21 13:00:35 +0900

    deploy.sh 내용 수정: grep 시 우리 프로젝트 명으로 찾게

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | grep sulsul | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
