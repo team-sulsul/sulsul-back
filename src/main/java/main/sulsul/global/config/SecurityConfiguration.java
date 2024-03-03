9e54e9e9df27dcb0e319034a8e5e5d8f7b16aefc
Seokju Hong <iamseokjuhong@gmail.com>
2024-03-04 00:18:07 +0900

    KAN-29 타임리프 구성해보기

--- src/main/java/main/sulsul/global/config/SecurityConfiguration.java
+++ src/main/java/main/sulsul/global/config/SecurityConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,10 +25,28 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(configurer -> configurer.ignoringRequestMatchers("/admin/**"))
+            .formLogin(form -> {
+                form.loginPage("/admin/login")
+                    .defaultSuccessUrl("/admin/home")
+                    .failureUrl("/admin/login?error=true")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .loginProcessingUrl("/admin/login");
+            })
             .securityMatcher("/admin/**")
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers("/admin/login").permitAll();
                 auth.anyRequest().authenticated();
             }).build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+            .username("somefood")
+            .password(passwordEncoder().encode("tjrwn12"))
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
