b44b7566917b34e4324be0ede3c535b4b5079b7f
Taemin Lee <dlrgks88@naver.com>
2024-03-28 14:48:20 +0900

    검증 카카오 로그인 서비스 테스트 위해 일시적으로 회원가입시 password와 role USER 부분 저장되도록 로직 추가

--- src/main/java/main/sulsul/oauth/application/OAuthLoginService.java
+++ src/main/java/main/sulsul/oauth/application/OAuthLoginService.java
@@ -3,12 +3,14 @@ package main.sulsul.oauth.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import main.sulsul.member.domain.Member;
+import main.sulsul.member.domain.Role;
 import main.sulsul.member.domain.dao.MemberRepository;
 import main.sulsul.oauth.domain.generator.AuthTokens;
 import main.sulsul.oauth.domain.generator.AuthTokensGenerator;
 import main.sulsul.oauth.domain.oauth.OAuthInfoResponse;
 import main.sulsul.oauth.domain.oauth.OAuthLoginParams;
 import main.sulsul.oauth.domain.oauth.RequestOAuthInfoService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -36,6 +38,8 @@ public class OAuthLoginService {
         Member member = Member.builder()
                 .email(oAuthInfoResponse.getEmail())
                 .username(oAuthInfoResponse.getNickname())
+                .role(Role.USER)
+                .password("1111")
                 .build();
         return memberRepository.save(member).getId();
     }
