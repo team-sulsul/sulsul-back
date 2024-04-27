bd7306a8f14c90e35d9cc771ea5d61639bb9f456
Taemin Lee <dlrgks88@naver.com>
2024-04-27 19:42:49 +0900

    로그인 고도화 - db에 username에 들어가는 값을 nickname이 아니라, email로 들어가게 해야되도록 수정. 유니크 값이 들어가야 되니.

--- src/main/java/main/sulsul/oauth/application/OAuthLoginService.java
+++ src/main/java/main/sulsul/oauth/application/OAuthLoginService.java
@@ -58,7 +58,7 @@ public class OAuthLoginService {
     private Long newMember(OAuthInfoResponse oAuthInfoResponse) {
         Member member = Member.builder()
                 .email(oAuthInfoResponse.getEmail())
-                .username(oAuthInfoResponse.getNickname())
+                .username(oAuthInfoResponse.getEmail())
                 .role(Role.USER)
                 .password(passwordEncoder().encode(SIMPLE_PASSWORD))
                 .build();
